22cc24419c54f862788aeb1c43227e012875821b
AgusQuintanar <agusquintanar17@gmail.com>
2019-07-18 14:44:02 -0500

    Improve conditions, now it only saves rows that have at least one 'S'

--- SaveInteractiveGrid.sql
+++ SaveInteractiveGrid.sql
@@ -16,16 +16,15 @@ begin
 
      when 'U' then 
       
-     IF var_id_repetido = 0 AND (:CONSULTA IS NOT NULL OR :EDIT IS NOT NULL OR :ALTA IS NOT NULL OR :CAN IS NOT NULL OR :PERMISO5 IS NOT NULL OR :PERMISO6 IS NOT NULL) THEN 
+     IF var_id_repetido = 0 AND (:CONSULTA == 'S' OR :EDIT == 'S' OR :ALTA == 'S' OR :CAN == 'S' OR :PERMISO5 == 'S' OR :PERMISO6 == 'S') THEN 
              insert into SEG_USUARIOS_OPC (SEG_USUARIOS_ID, SEG_OPCIONES_ID, CONSULTA, EDIT, ALTA, CAN, PERMISO5, PERMISO6) 
-               values (:P23_ID, :ID, :CONSULTA, :EDIT, :ALTA, :CAN, :PERMISO5, :PERMISO6 );
+               values (:P23_ID, :ID, :CONSULTA, :EDIT, :ALTA, :CAN, :PERMISO5, :PERMISO6);
                
-     ELSIF var_id_repetido = 1 AND (:CONSULTA IS NULL AND :EDIT IS NULL AND :ALTA IS NULL AND :CAN IS NULL AND :PERMISO5 IS NULL AND :PERMISO6 IS  NULL) THEN 
+     ELSIF var_id_repetido = 1 AND (:CONSULTA <> 'S' AND :EDIT <> 'S' AND :ALTA <> 'S' AND :CAN <> 'S' AND :PERMISO5 <> 'S' AND :PERMISO6 <> 'S') THEN 
        delete SEG_USUARIOS_OPC
        where SEG_USUARIOS_ID  = :P23_ID
        AND SEG_OPCIONES_ID = :ID; 
              
-     
      ELSE 
       update SEG_USUARIOS_OPC 
             set  
