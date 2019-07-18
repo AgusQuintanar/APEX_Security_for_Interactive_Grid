c54a2c0db9afbc5360995cd67e5d84420f0fc810
AgusQuintanar <agusquintanar17@gmail.com>
2019-07-18 14:17:02 -0500

    updated script to work in a static interactive grid

--- SaveInteractiveGrid.sql
+++ SaveInteractiveGrid.sql
@@ -1,4 +1,12 @@
+declare
+  var_id_repetido number;
+
 begin 
+     select count(*)
+     into   var_id_repetido
+     from   SEG_USUARIOS_OPC
+     where  SEG_OPCIONES_ID = :ID;
+     
      case :APEX$ROW_STATUS 
      when 'C' then -- Note: In EA2 this has been changed from I to C for consistency with Tabular Forms
      
@@ -6,8 +14,20 @@ begin
         values (:P23_ID, :ID, :CONSULTA, :EDIT, :ALTA, :CAN, :PERMISO5, :PERMISO6 ) 
         returning ROWID into :ROWID; 
 
-     when 'U' then         
-         update SEG_USUARIOS_OPC 
+     when 'U' then 
+      
+     IF var_id_repetido = 0 AND (:CONSULTA IS NOT NULL OR :EDIT IS NOT NULL OR :ALTA IS NOT NULL OR :CAN IS NOT NULL OR :PERMISO5 IS NOT NULL OR :PERMISO6 IS NOT NULL) THEN 
+             insert into SEG_USUARIOS_OPC (SEG_USUARIOS_ID, SEG_OPCIONES_ID, CONSULTA, EDIT, ALTA, CAN, PERMISO5, PERMISO6) 
+               values (:P23_ID, :ID, :CONSULTA, :EDIT, :ALTA, :CAN, :PERMISO5, :PERMISO6 );
+               
+     ELSIF var_id_repetido = 1 AND (:CONSULTA IS NULL AND :EDIT IS NULL AND :ALTA IS NULL AND :CAN IS NULL AND :PERMISO5 IS NULL AND :PERMISO6 IS  NULL) THEN 
+       delete SEG_USUARIOS_OPC
+       where SEG_USUARIOS_ID  = :P23_ID
+       AND SEG_OPCIONES_ID = :ID; 
+             
+     
+     ELSE 
+      update SEG_USUARIOS_OPC 
             set  
                 CONSULTA = :CONSULTA,
                 EDIT = :EDIT,
@@ -18,6 +38,10 @@ begin
                 
           where SEG_USUARIOS_ID  = :P23_ID
           AND SEG_OPCIONES_ID = :ID; 
+     
+     END IF; 
+     
+         
      when 'D' then 
          delete SEG_USUARIOS_OPC
          where SEG_USUARIOS_ID  = :P23_ID
